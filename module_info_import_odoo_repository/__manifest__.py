ab448ed6e944e136e7142c9abf5a5dc504597c79
Sébastien BEAU <sebastien.beau@akretion.com>
2025-10-16 10:09:40 +0200

    [ADD] module_info_import_odoo_repository: add module

---
+++ module_info_import_odoo_repository/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Module Info Import From odoo Repository",
+    "summary": "Get information about modules from akretion/odoo-module-tracker",
+    "version": "16.0.1.0.0",
+    "website": "https://github.com/akretion/partner-module-information",
+    "license": "AGPL-3",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "category": "Tools",
+    "depends": ["module_info_import"],
+    "data": ["data/ir_cron.xml"],
+    "installable": True,
+}
---
+++ module_info_import_odoo_repository/models/module_version.py
@@ -0,0 +1,11 @@
+# Copyright 2025 Akretion (https://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ModuleVersion(models.Model):
+    _inherit = "module.version"
+
+    migrations = fields.Json(helper="Migration information from odoo repository")
