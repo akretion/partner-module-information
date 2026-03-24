d039dcab4683c73e49ce7103d9e6ee8baf6b824c
Florian da Costa <florian.dacosta@akretion.com>
2026-03-24 12:21:55 +0100

    [MIG] module_info_pull_request_timesheet to 18

--- module_info_pull_request_timesheet/__manifest__.py
+++ module_info_pull_request_timesheet/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Module Info Timesheet",
     "summary": "Add timesheet to Pull request",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Akretion, Odoo Community Association (OCA)",
     "website": "https://github.com/akretion/partner-module-information",
--- module_info_pull_request_timesheet/models/pull_request.py
+++ module_info_pull_request_timesheet/models/pull_request.py
@@ -8,5 +8,7 @@ _logger = logging.getLogger(__name__)
 class PullRequest(models.Model):
     _inherit = "pull.request"
 
-    allow_timesheets = fields.Boolean(related="project_id.allow_timesheets")
+    allow_timesheets = fields.Boolean(
+        related="project_id.allow_timesheets", string="Allow Timesheets"
+    )
     timesheet_ids = fields.One2many("account.analytic.line", "pr_id", "Timesheets")
